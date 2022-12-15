e1fc2e4e0fe73fb3d7cf106ebad641961627ebe4
Aleksandr <aleksandr.evlanicov@gmail.com>
2022-12-15 14:55:34 +0300

    Dz3

---
+++ Dz3/Program.cs
@@ -0,0 +1,33 @@
+﻿int[] myArray = {35, 71, 223, 21, 78};
+
+int min_N = myArray[0];
+int max_N = 0;
+int result = 0;
+
+
+
+for ( int i = 0; i < myArray.Length; i++)
+{
+    if (myArray[i] < min_N)
+    {
+        min_N = myArray[i];
+        
+    }
+}
+
+
+for ( int i = 0; i < myArray.Length; i++)
+{
+    if (myArray[i] > max_N)
+    {
+        max_N = myArray[i];
+    
+    }
+}
+result = max_N - min_N; 
+Console.WriteLine(result );
+Console.ReadLine();
+
+
+
+
